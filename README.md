ccd8d1681c515d441c0f3e738fb4350c8081d16a
Bora Bilgin <bora@bilg.in>
2020-05-16 12:01:46 -0400

    Added no-sandbox option to Pupppeteer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # swot-analysis-poc
 SWOT Analysis PoC using Python and Octave backend
+
+
+Server will need to run puppeteer. For Ubuntu, please install:
+
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev
+sudo apt-get install libgconf-2-4 libnss3 libxss1
+sudo apt-get install libasound2 libxtst6 xauth xvfb
+sudo apt-get install libgbm-dev
--- src/utils/report.ts
+++ src/utils/report.ts
@@ -87,7 +87,7 @@ export class AnalysisReport {
     console.log("HTML report size is", html.length);
     //for debugging, save the html file
     //Fs.writeFileSync(Path.resolve(report.folder, report.filename + "-test.html"), html)
-    const browser = await Puppeteer.launch()
+    const browser = await Puppeteer.launch({args: ['--no-sandbox'})
     const page = await browser.newPage()
     await page.setContent(html)
 
